bd0b408d2dd2edbdf2eba4b9af0e5fdb986ddd15
Ross Morgan <morgan.ross.512@gmail.com>
2022-10-12 17:38:08 +0100

    change patterns to defaultdict

--- src/demarcation/highlighter.py
+++ src/demarcation/highlighter.py
@@ -1,10 +1,10 @@
 import re
+from collections import defaultdict
 from dataclasses import dataclass
 
 from PyQt6 import QtCore, QtGui
 
 from .logger import AppLogger
-
 
 logger = AppLogger("logger")
 
@@ -44,47 +44,27 @@ def pattern(p: str) -> re.Pattern:
 
 
 def new_highlighter() -> BMLHighlighter:
-    patterns: dict[str, PatternFormat] = {
-        "for": None,
-        "exists": None,
-        "matrix": None,
-        "and": None,
-        "set": None,
-        "func": None,
-        "subset": None,
-        "superset": None,
-        "union": None,
-        "intersection": None,
-        "difference": None,
-        "mod": None,
-        "√": None,
-        "+": None,
-        "-": None,
-        "*": None,
-        "/": None,
-        "`sym": None,
-        ",": None,
-        "paren": None,
-        "set_name": None,
-        "arrow": None,
-        "mod": None,
-    }
+    patterns = defaultdict(PatternFormat)  # noqa
 
-    for kwd in patterns:
-        if patterns[kwd] is None:
-            patterns[kwd] = PatternFormat()
-
+    patterns["variable"].pattern = pattern(r"[A-Za-z]+")
+    patterns["matrix"].pattern = pattern(r"(?<!\S)matrix(?!\w+)")
     patterns["set"].pattern = pattern(r"(?<!\S)set(?!\w+)")
-    patterns["func"].pattern = pattern(r"(?<!\S)\w+(?=\(\w+\*)")
+    patterns["func"].pattern = pattern(r"\w+(?=\(\w+\))")
     patterns["and"].pattern = pattern(r"(?<!\S)and(?!\w+)")
     patterns["for"].pattern = pattern(r"(?<!\S)for(?=\s*)")
     patterns["exists"].pattern = pattern(r"(?<!\S)exists(?!\w+)")
     patterns["paren"].pattern = pattern(r"[()\[\]{}]")
     patterns["`sym"].pattern = pattern(r"(?<!\S)`\w+")
+    patterns["matrix_name"].pattern = pattern(r"(?<=^matrix )\w+")
     patterns["set_name"].pattern = pattern(r"(?<=^set )\w+")
     patterns["arrow"].pattern = pattern(r"(?<!\S)=>(?!\S)")
     patterns["mod"].pattern = pattern(r"(?<!\S)mod(?!\w+)")
+    patterns["comment"].pattern = pattern(r"#.*$")
+    patterns["number"].pattern = pattern(r"\d+\.?\d*[a-z]?")
+    patterns["abs"].pattern = pattern(r"\|")
 
+    patterns["variable"].text_format = fmt(COLOUR.cyan)
+    patterns["matrix"].text_format = fmt(COLOUR.darkCyan)
     patterns["set"].text_format = fmt(COLOUR.darkCyan)
     patterns["func"].text_format = fmt(COLOUR.darkCyan)
     patterns["and"].text_format = fmt(COLOUR.cyan)
@@ -92,13 +72,17 @@ def new_highlighter() -> BMLHighlighter:
     patterns["exists"].text_format = fmt(COLOUR.red)
     patterns["paren"].text_format = fmt(COLOUR.yellow)
     patterns["`sym"].text_format = fmt(COLOUR.green)
+    patterns["matrix_name"].text_format = fmt(COLOUR.green)
     patterns["set_name"].text_format = fmt(COLOUR.green)
     patterns["arrow"].text_format = fmt(COLOUR.gray)
     patterns["mod"].text_format = fmt(COLOUR.red)
+    patterns["comment"].text_format = fmt(COLOUR.lightGray)
+    patterns["number"].text_format = fmt(COLOUR.cyan)
+    patterns["abs"].text_format = fmt(COLOUR.gray)
 
     syntax_highlighter = BMLHighlighter()
 
-    for kwd, f in patterns.items():
+    for _, f in patterns.items():
         if f.pattern is None or f.text_format is None:
             continue
 
